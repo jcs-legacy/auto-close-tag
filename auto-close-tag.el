df92d9bddd03dfa56f3fbdb38087d3ce2ebf93c4
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-17 02:55:43 +0800

    chore(eask): info

--- auto-close-tag.el
+++ auto-close-tag.el
@@ -4,11 +4,10 @@
 ;; Created date 2018-12-04 17:04:50
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; Description: Automatically add HTML/XML close tag.
-;; Keyword: keybindings
+;; URL: https://github.com/jcs-elpa/auto-close-tag
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.6"))
-;; URL: https://github.com/jcs-elpa/auto-close-tag
+;; Keywords: keybindings
 
 ;; This file is NOT part of GNU Emacs.
 
